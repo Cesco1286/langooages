64a53e98817fc195eba0b3d4c5a569bbd2c3ed2a
francesco <fra.delussu@libero.it>
2017-11-09 23:14:45 +0100

    created net project and added first user module

---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+namespace langooages.Models{
+    public class User{
+        public long Id {get;set;}
+        public string Username{get;set;}
+        public string Password{get;set;}
+        public int Progress{get;set;}
+    }
+}
